6e15a705b78f80e95817edd1e6b8a345fffc4259
zach <Frenchzj@s.dcsdk12.org>
2022-10-11 16:16:18 -0600

    challenge 2 work

--- scripts/challenge-2.js
+++ scripts/challenge-2.js
@@ -16,3 +16,22 @@ if (!output) throw "Output container not found.";
 
 //@ts-check
 // your code starts after this line
+
+var result = 'x';
+for(var i = 0; i < 11; i++) {
+	for (var j = 0; j < 11; j++) {
+		if(i == 0 && j > 0){
+			result += '[' + j + ']';
+		}
+		else if( j == 0 && i > 0) {
+			result += '[' + i + ']';
+
+		}
+		else if (i > 0 ** j > 0) {
+			result += (i*j) + ' ';
+				}
+	}
+	result += '/n'
+	output.append(result);
+}
+
